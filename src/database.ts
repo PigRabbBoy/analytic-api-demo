86ef1e6503fb02550794e66ef6ba9e0c83845e3c
PigRabb Boy <sirimongkon.se@gmail.com>
2025-03-03 17:39:23 +0700

    update analytic api with user

--- src/database.ts
+++ src/database.ts
@@ -1,13 +1,18 @@
-import {
-  int,
-  sqliteTable,
-  text,
-  blob,
-} from "drizzle-orm/sqlite-core";
+import { int, sqliteTable, text, blob } from "drizzle-orm/sqlite-core";
+
+export const userTable = sqliteTable("user", {
+  ohoId: text().primaryKey(),
+  userId: text(),
+  data: blob({ mode: "json" }),
+});
 
 export const analyticTable = sqliteTable("analytic", {
   id: text().primaryKey(),
-  userId: text("user_id").notNull(),
+  ohoPixel: text("oho_pixel").notNull(),
+  clientId: text("user_id").notNull(),
+  type: text(),
+  user: blob({ mode: "json" }),
   data: blob({ mode: "json" }),
+  timestamp: text(),
   createdAt: text("created_at"),
 });
--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,15 @@ import { Elysia, t } from "elysia";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { Database } from "bun:sqlite";
 import { randomUUIDv7 } from "bun";
-import { analyticTable } from "./database";
+import { analyticTable, userTable } from "./database";
 import { swagger } from "@elysiajs/swagger";
-import { desc } from "drizzle-orm";
+import { desc, eq } from "drizzle-orm";
 
 const sqlite = new Database("database.db");
-const db = drizzle({ client: sqlite });
+const db = drizzle({
+  client: sqlite,
+  schema: { user: userTable, analytic: analyticTable },
+});
 
 const app = new Elysia();
 
@@ -15,29 +18,104 @@ app.use(swagger());
 
 app.get("/", () => "Hello Elysia");
 
+app.get("token", async () => {
+  const ohoUserId = randomUUIDv7();
+
+  await db.insert(userTable).values({
+    ohoId: ohoUserId,
+  });
+  const token = btoa(ohoUserId);
+
+  return { token, ohoUserId };
+});
+
+app.post(
+  "auth",
+  async (req) => {
+    const body = req.body;
+
+    let token = body.ohoToken;
+    let ohoUserId: string;
+    if (token) {
+      ohoUserId = atob(token);
+      const user = await db.query.user.findFirst({
+        where: eq(userTable.ohoId, ohoUserId),
+      });
+
+      if (!user) {
+        return req.error("Not Found", "user not found");
+      }
+
+      if (user.userId && user.userId !== body.userId) {
+        return req.error("Conflict", "user already auth");
+      }
+
+      const auth = await db.query.user.findFirst({
+        where: eq(userTable.userId, body.userId),
+      });
+
+      if (auth && auth.userId !== body.userId) {
+        return req.error("Conflict", "user already auth");
+      }
+
+      await db
+        .update(userTable)
+        .set({
+          userId: body.userId,
+          data: body.data,
+        })
+        .where(eq(userTable.ohoId, ohoUserId));
+    } else {
+      ohoUserId = randomUUIDv7();
+      token = btoa(ohoUserId);
+
+      await db.insert(userTable).values({
+        ohoId: ohoUserId,
+        userId: body.userId,
+        data: body.data,
+      });
+    }
+
+    return { token, ohoUserId };
+  },
+  {
+    body: t.Object({
+      userId: t.String({ description: "ไอดี user ของ website" }),
+      ohoToken: t.Optional(t.String({ description: "token ของ oho" })),
+      data: t.Optional(t.Any({ description: "ข้อมูล user ของ website" })),
+    }),
+  }
+);
+
+const analyticObj = t.Object({
+  type: t.String(),
+  ohoPixel: t.String(),
+  clientId: t.String(),
+  user: t.Optional(t.Any()),
+  data: t.Optional(t.Any()),
+  timestamp: t.String(),
+});
+
 app.post(
   "/analytic",
   async (req) => {
     const body = req.body;
-    const data = Array.isArray(body.data) ? body.data : [body.data];
-    const createdAt = new Date().toISOString()
+    const data = body.list;
+    if (!data) return { status: "data not found" };
+    const createdAt = new Date().toISOString();
 
     await db.insert(analyticTable).values(
       data.map((v) => ({
         id: randomUUIDv7(),
-        userId: body.userId,
-        data: v,
         createdAt,
+        ...v,
       }))
     );
     return { status: "ok" };
   },
   {
     body: t.Object({
-      userId: t.String({
-        description: "ไอดีของ user",
-      }),
-      data: t.Any({ description: "ข้อมูลที่ต้องการเก็บ" }),
+      list: t.Array(analyticObj, { minItems: 1 }),
     }),
   }
 );
@@ -55,7 +133,7 @@ app.get(
       .from(analyticTable)
       .limit(limit)
       .offset(offset)
-      .orderBy(desc(analyticTable.createdAt));
+      .orderBy(desc(analyticTable.timestamp));
 
     return {
       data,
@@ -63,8 +141,8 @@ app.get(
   },
   {
     query: t.Object({
-      page: t.Number({ description: "หน้า" }),
-      pageSize: t.Number({ description: "จำนวนต่อหน้า" }),
+      page: t.Number({ description: "หน้า", default: 1 }),
+      pageSize: t.Number({ description: "จำนวนต่อหน้า", default: 20 }),
     }),
   }
 );
